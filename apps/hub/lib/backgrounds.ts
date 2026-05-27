42f8a10c740b35467c954e107d273994dd82ca3f
Terumasa0218 <p.sh.ine.46315@gmail.com>
2026-05-28 00:17:09 +0900

    Add antique CPU battle UI foundation

--- apps/hub/lib/backgrounds.ts
+++ apps/hub/lib/backgrounds.ts
@@ -1,6 +1,6 @@
 export const BACKGROUNDS = {
   home: '/images/home13.png',
-  battle: '/images/battle1.png',
+  battle: '/assets/ui/antique/wood-table.png',
   settings: '/images/bg-home-04.png',
 } as const;
 
